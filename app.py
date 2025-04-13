6daf58cf29b7d18e68595c3f723acc16b4894a49
youssef mahmoud <99423758+Youseef0123@users.noreply.github.com>
2025-04-13 14:47:43 +0200

    tesst

--- app.py
+++ app.py
@@ -479,7 +479,11 @@ def predict():
         }), 500
 
 if __name__ == "__main__":
-    # Run the app
-    # استخدام منفذ من متغير البيئة أو 5000 كقيمة افتراضية
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
+      
+
+
+
+
+      
